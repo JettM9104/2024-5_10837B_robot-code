30b62332d81f6b8cae8ba0b98746d7c24f7d748b
JMu <jettmuu@gmail.com>
2024-11-16 13:23:04 -0500

    added shooting motor logic

--- Robot 4/Driver.cpp
+++ Robot 4/Driver.cpp
@@ -32,8 +32,9 @@ controller Controller = controller();
 motor ApositiveU = motor(PORT1, true);
 motor BpositiveR = motor(PORT6, false);
 motor AnegativeD = motor(PORT7, true);
-motor bNegativeL = motor(PORT12, false);
-
+motor bNegativeL = motor(PORT12, false)
+motor shooting1 = motor(PORT2, false);
+motor shooting2 = motor(PORT3, true);
 
 // generating and setting random seed
 void initializeRandomSeed(){
@@ -95,6 +96,7 @@ namespace robot {
   }
   namespace bypass {
     bool driving = false; //bypass for driving
+    bool shooting = false; //bypass for conveyer-catapult motorshare
   }
   namespace constants {
     int maxMotorSpeed = 100;
@@ -151,6 +153,24 @@ int main() {
       BpositiveR.stop();
       AnegativeD.stop();
       bNegativeL.stop();
+    }
+
+    if (!robot::bypass::shooting) {
+      if (Controller.ButtonLUp.pressing() && !(Controller.ButtonLUp.pressing() && Controller.ButtonLDown.pressing())) {
+        shooting1.spin(forward, 100, percent);
+        shooting2.spin(forward, 100, percent);
+      }
+      else if (Controller.ButtonLDown.pressing() && !(Controller.ButtonLUp.pressing() && Controller.ButtonLDown.pressing())) {
+        shooting1.spin(reverse, 100, percent);
+        shooting2.spin(forward, 100, percent);
+      }
+      else if (Controller.ButtonLUp.pressing() && Controller.ButtonLDown.pressing()) {
+        Brain.playSound(fillup);
+      }
+      else {
+        shooting1.stop();
+        shooting2.stop();
+      }
     }
   }
 
