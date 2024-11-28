3bdb67930a37efb47f8ebfcb2df937f2ca11f99a
JMu <jettmuu@gmail.com>
2024-11-27 20:13:09 -0500

    Adjsuted and a/ overwritten
    00032

--- Robot 4/Auton.cpp
+++ Robot 4/Auton.cpp
@@ -28,17 +28,11 @@ brain Brain;
 
 // Robot configuration code.
 inertial BrainInertial = inertial();
-controller Controller = controller();
 motor ApositiveU = motor(PORT1, true);
 motor BpositiveR = motor(PORT6, false);
 motor AnegativeD = motor(PORT7, true);
 motor BnegativeL = motor(PORT12, false);
-motor shooting1 = motor(PORT4, false);
-motor shooting2 = motor(PORT10, true);
-pneumatic cats = pneumatic(PORT5);
-pneumatic dogs = pneumatic(PORT11);
-gyro turning = gyro(PORT3);
-distance conveyerSensor = distance(PORT9);
+controller Controller = controller();
 
 
 // generating and setting random seed
@@ -63,6 +57,10 @@ void vexcodeInit() {
   initializeRandomSeed(); 
 }
 
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
 #pragma endregion VEXcode Generated Robot Configuration
 
 //----------------------------------------------------------------------------
@@ -79,7 +77,6 @@ void vexcodeInit() {
 void drive(double dir, double dist, double k, double kp, double ki, double kd, double timeout);
 void turn(double target, double kp, double ki, double kd, double timeout);
 void run(); // The code in this function run while the main code is running
-void init();
 
 // Namespaces for variable organization
 namespace robot {
@@ -124,7 +121,6 @@ namespace robot {
       double threshold = 5,  integralResetZone = 3,  maxSpeed = 100;
       double maximum = 1;
       double dirRad;
-      double correctionK = 0.006;
 
       namespace u {
         double error = 0, integral = 0, derivative = 0;
@@ -156,13 +152,12 @@ int main() {
 
   vexcodeInit();
 
-  init();
-
-  thread direction = thread(run);
-
-  
+  thread myThread = thread(run);
   BrainInertial.setHeading(0,degrees);
-  drive(90,3000,100,1,0.1,0.1,0);
+  drive(90,300,100,1,0.1,0.1,0);
+  drive(0,300,100,1,0.1,0.1,0);
+  drive(-90,300,100,1,0.1,0.1,0);
+  drive(180,300,100,1,0.1,0.1,0);
 }
 
 // Defenition of run()
@@ -174,18 +169,10 @@ void run() {
   }
 }
 
-void init() {
-  ApositiveU.setStopping(hold);
-  BpositiveR.setStopping(hold);
-  AnegativeD.setStopping(hold);
-  BnegativeL.setStopping(hold);
-}
 // Definiton of drive()
 void drive(double dir, double dist, double k, double kp, double ki, double kd, double timeout) {
   Brain.Timer.reset();
   
-  double begin = robot::angl::limrot;
-
   robot::auton::pid::dirRad = dir * robot::constants::pi / 180.0;
   robot::command::a = cos(robot::auton::pid::dirRad); //forward backward
   robot::command::b = sin(robot::auton::pid::dirRad); //strafing
@@ -221,22 +208,6 @@ void drive(double dir, double dist, double k, double kp, double ki, double kd, d
   robot::drivet::l /= robot::auton::pid::maximum;
 
   while (true) {
-    robot::drivet::u = robot::command::a + robot::command::b + robot::command::c;
-    robot::drivet::r = robot::command::a - robot::command::b - robot::command::c;
-    robot::drivet::d = robot::command::a - robot::command::b + robot::command::c;
-    robot::drivet::l = robot::command::a + robot::command::b - robot::command::c;
-
-    robot::auton::pid::maximum = robot::drivet::u > robot::auton::pid::maximum ? robot::drivet::u : robot::auton::pid::maximum;
-    robot::auton::pid::maximum = robot::drivet::r > robot::auton::pid::maximum ? robot::drivet::r : robot::auton::pid::maximum;
-    robot::auton::pid::maximum = robot::drivet::d > robot::auton::pid::maximum ? robot::drivet::d : robot::auton::pid::maximum;
-    robot::auton::pid::maximum = robot::drivet::l > robot::auton::pid::maximum ? robot::drivet::l : robot::auton::pid::maximum;
-
-    robot::drivet::u /= robot::auton::pid::maximum;
-    robot::drivet::r /= robot::auton::pid::maximum;
-    robot::drivet::d /= robot::auton::pid::maximum;
-    robot::drivet::l /= robot::auton::pid::maximum;
-
-
     robot::auton::pid::u::error = dist - fabs(ApositiveU.position(degrees));
     robot::auton::pid::r::error = dist - fabs(BpositiveR.position(degrees));
     robot::auton::pid::d::error = dist - fabs(AnegativeD.position(degrees));
@@ -246,18 +217,14 @@ void drive(double dir, double dist, double k, double kp, double ki, double kd, d
     robot::auton::pid::r::integral = fabs(robot::auton::pid::r::error) > robot::auton::pid::integralResetZone ? robot::auton::pid::r::integral + robot::auton::pid::r::error : 0;
     robot::auton::pid::d::integral = fabs(robot::auton::pid::d::error) > robot::auton::pid::integralResetZone ? robot::auton::pid::d::integral + robot::auton::pid::d::error : 0;
     robot::auton::pid::l::integral = fabs(robot::auton::pid::l::error) > robot::auton::pid::integralResetZone ? robot::auton::pid::l::integral + robot::auton::pid::l::error : 0;
-    
+
     robot::auton::pid::u::derivative = robot::auton::pid::u::error - robot::auton::pid::u::lastError;
     robot::auton::pid::r::derivative = robot::auton::pid::r::error - robot::auton::pid::r::lastError;
     robot::auton::pid::d::derivative = robot::auton::pid::d::error - robot::auton::pid::d::lastError;
     robot::auton::pid::l::derivative = robot::auton::pid::l::error - robot::auton::pid::l::lastError;
     
-    robot::command::c = (robot::angl::limrot) * robot::auton::pid::correctionK * -1; 
-
-    printf("C Command: %f\n", robot::command::c);
-    
     if (((robot::auton::pid::u::error)+(robot::auton::pid::r::error)+(robot::auton::pid::d::error)+(robot::auton::pid::l::error)/(4)) < robot::auton::pid::threshold) {break; }
-    if ((timeout < Brain.Timer.value()) && (timeout != 0)) {break; }
+    if ((timeout > Brain.Timer.value()) && (timeout != 0)) {break; }
 
     robot::auton::pid::u::motorSpeed = (robot::auton::pid::u::error * robot::constants::pid::kp + robot::auton::pid::u::integral * robot::constants::pid::ki + robot::auton::pid::u::derivative * robot::constants::pid::kd) * robot::drivet::u;
     robot::auton::pid::r::motorSpeed = (robot::auton::pid::r::error * robot::constants::pid::kp + robot::auton::pid::r::integral * robot::constants::pid::ki + robot::auton::pid::r::derivative * robot::constants::pid::kd) * robot::drivet::r;
@@ -362,23 +329,6 @@ void turn(double target, double kp, double ki, double kd, double timeout) {
   }
 }
 
-void liftMacro() {
-
-  cats.retract(cylinder2);
-  dogs.extend(cylinder2);
-  shooting1.spin(forward, 100, percent);
-  shooting2.spin(forward, 100, percent);
-  while (!(conveyerSensor.objectDistance(mm) < 40)) {wait(20, msec); }
-
-  shooting1.stop();
-  shooting2.stop();
-  dogs.retract(cylinder2);
-    
-  wait(1500, msec);
-
-  dogs.extend(cylinder2);
-
-}
 
 
 // Namespace Tree for better organization
