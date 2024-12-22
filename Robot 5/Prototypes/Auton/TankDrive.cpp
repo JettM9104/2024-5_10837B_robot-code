e95885ca56b61cb1b6c073955b615676876b289a
JettM9104 <64600882+JettM9104@users.noreply.github.com>
2024-12-22 18:27:25 -0500

    Created curve fucntion
    00080

--- Robot 5/Prototypes/Auton/TankDrive.cpp
+++ Robot 5/Prototypes/Auton/TankDrive.cpp
@@ -6,14 +6,12 @@
 #include <math.h>
 #include <string.h>
 
-
 #include "vex.h"
 
 using namespace vex;
 
 // Brain should be defined by default
 brain Brain;
-
 
 // START IQ MACROS
 #define waitUntil(condition)                                                   \
@@ -25,13 +23,11 @@ brain Brain;
   for (int iterator = 0; iterator < iterations; iterator++)
 // END IQ MACROS
 
-
 // Robot configuration code.
 inertial BrainInertial = inertial();
 controller Controller = controller();
 motor leftDrivetrain = motor(PORT1, true);
 motor rightDrivetrain = motor(PORT2, false);
-
 
 // generating and setting random seed
 void initializeRandomSeed(){
@@ -48,7 +44,6 @@ void initializeRandomSeed(){
 }
 
 
-
 void vexcodeInit() {
 
   // Initializing random seed.
@@ -57,14 +52,16 @@ void vexcodeInit() {
 
 #pragma endregion VEXcode Generated Robot Configuration
 
-
 // Function Declaration
 void init();
+
+// Enum for curve function
+enum dire { l, r };
 
 // Movement Functions
 void drive(double distance, double timeout = 0, directionType dir = forward); // Distance in Units Declared in function, Timeout in Seconds
 void turn(double angle, double timeout = 0, directionType dir = forward); // Angle in Degrees, Timeout in Seconds
-
+void curve(double theta, double radius, double timeout, dire dir = r); 
 
 // Namespaces for organization of PID Coefficients
 namespace pid
@@ -72,12 +69,14 @@ namespace pid
     namespace drive { float kP, kI, kD; }
     namespace turn { float kP, kI, kD; }
     namespace correction { float kP, kI, kD; }
+    namespace curve { float kP, kI, kD; }
 }
 
 // Refrences for ease of access of variables
 float& dkP = pid::drive::kP, dkI = pid::drive::kI, dkD = pid::drive::kD;
 float& tkP = pid::turn::kP, tkI = pid::turn::kI, tkD = pid::turn::kD;
 float& ckP = pid::correction::kP, ckI = pid::correction::kI, ckD = pid::correction::kD;
+float& ukP = pid::curve::kP, ukI = pid::curve::kI, ukD = pid::curve::kD;
 
 // PIEEEEEEEEEEEE!!
 const double pi = 3.1415926;
@@ -90,7 +89,6 @@ int main() {
   drive(20, 3, reverse);
   drive(20);
 }
-
 
 void init() {
   // initalize stuff here, for example, setstopping to hold, coast, or brake
@@ -105,7 +103,6 @@ void drive(double distance, double timeout, directionType dir) { // Drive Functi
   double error, integral = 0, derivative;
   double lastError = 0;
   double leftSpeed, rightSpeed;
-
 
   // Coefficients for PID Correcion System
   double correctionError, correctionIntegral = 0, correctionDerivative, correctionLastError = 0;
@@ -177,8 +174,8 @@ void turn(double angle, double timeout, directionType dir) {
   // Wheel Distance Calculation
   double wheelCircum = 200;
   double gearRatio = 2 / 1;
-  double wheelBase = 254; // Must be same units as wheelCircum
-  double goalDegrees =  (angle / 360) * pi * 10 * wheelBase / 360 * 360 / wheelCircum / gearRatio;
+  double trackWidth = 254; // Must be same units as wheelCircum
+  double goalDegrees =  (angle / 360) * pi * 10 * trackWidth / 360 * 360 / wheelCircum / gearRatio;
 
   
   // Reset Motor Encoder Positions
@@ -210,3 +207,66 @@ void turn(double angle, double timeout, directionType dir) {
     wait(20, msec); // Wait so the brain doesnt explode
   }
 }
+
+void curve(double theta, double radius, double timeout, dire dir) {
+  // Direction Parameter
+  if (dir == l) { theta *= 1; }
+
+  // Coefficients for PID Drive System
+  double Lerror, Lintegral = 0, Lderivative;
+  double LlastError = 0;
+  double LmotorSpeed;
+
+  double Rerror, Rintegral = 0, Rderivative;
+  double RlastError = 0;
+  double RmotorSpeed;
+
+  double threshold = 5, integralResetZone = 3;
+
+  // Saving the values so we don't have to reset the values in the beginning
+  double beginTimer = Brain.Timer.value();
+
+  // Wheel Distance Calculation
+  double wheelCircum = 200;
+  double gearRatio = 2 / 1;
+  double trackWidth = 254; // Must be same units as wheelCircum
+  double Lgoal = (theta / 360) * (pi * 2 * (radius - (trackWidth / 2))) * 360 / wheelCircum / gearRatio;
+  double Rgoal = (theta / 360) * (pi * 2 * (radius + (trackWidth / 2))) * 360 / wheelCircum / gearRatio;
+
+  // Reset Motor Encoder Positions
+  leftDrivetrain.resetPosition();
+  rightDrivetrain.resetPosition();
+
+  while (true) {
+    // Calculate PID Values
+    Lerror = Lgoal - (leftDrivetrain.position(degrees));
+    Lintegral = Lerror < integralResetZone ? 0 : Lintegral + Lerror;
+    Lderivative = Lerror - LlastError;
+    Rerror = Rgoal - (rightDrivetrain.position(degrees));
+    Rintegral = Rerror < integralResetZone ? 0 : Rintegral + Rerror;
+    Rderivative = Rerror - RlastError;
+
+    if (fabs(Lerror) < 3) { Lintegral = 0; } // Reset integral when target is almost met
+    if (fabs(Rerror) < 3) { Rintegral = 0; }
+
+    // Calculate Motor Speed
+    LmotorSpeed = ((Lerror * ukP) + (Lintegral * ukI) + (Lderivative * ukD));
+    RmotorSpeed = ((Rerror * ukP) + (Rintegral * ukI) + (Rderivative * ukD));
+
+    // Spin Motors
+    leftDrivetrain.spin(forward, LmotorSpeed, percent);
+    rightDrivetrain.spin(reverse, RmotorSpeed, percent);
+
+    // Exit Conditions
+    if ((fabs(Lerror) + fabs(Rerror) / 2 < threshold)) { break; }
+    if ((((Brain.Timer.value()) - beginTimer) > timeout) && (timeout != 0)) { break; }
+
+    // Set lastError Variable
+    LlastError = Lerror;
+    RlastError = Rerror;
+
+    wait(20, msec); // Wait so the brain doesnt explode
+  }
+}
+
+
