e8024766217e6fa222f4a7e503fd38c4a9db5d4d
JMu <64600882+JettM9104@users.noreply.github.com>
2025-04-08 16:42:43 -0400

    Created file directory for Robot v8
    00134

---
+++ Robot 8/Auton/vex.h
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <functional>
+
+namespace vex {
+
+enum port {PORT1, PORT2, PORT3, PORT4, PORT5, PORT6, PORT7, PORT8, PORT9, PORT10, PORT11, PORT12, PORTNULL};
+enum axisType {xaxis, yaxis, zaxis};
+enum timeUnit {msec, seconds};
+enum directionType {forward, reverse};
+enum speedUnit {rpm, percent};
+enum cylinder {cylinder1, cylinder2};
+enum degreesUnits {degrees};
+enum stopping {coast, brake, hold};
+enum distanceUnits {mm, inches};
+enum sound {siren};
+
+// Class representing an inertial sensor
+class inertial {
+private:
+  port portnumber;
+public:
+  inertial(port portnumber) {
+    this->portnumber = portnumber;
+  }
+  inertial() = default;
+
+  double acceleration(axisType axis) { 
+    // Return a dummy value for acceleration (this would interact with actual hardware)
+    return 0.0; 
+  }
+
+  void setHeading(double angle, degreesUnits unit) {}
+
+  void setRotation(double angle, degreesUnits unit) {}
+  double heading(degreesUnits unit) { return 0.0; }
+  double rotation(degreesUnits unit) { return 0.0; }
+  double value() { return 0.0; }
+};
+
+// Class representing a button
+class button {
+private:
+  std::function<void()> onPressCallback; // Store the callback function
+
+public:
+  bool pressing() { 
+    // Dummy implementation, you can add real logic for pressing detection
+    return false; 
+  }
+
+  bool pressed(std::function<void()> callback) {
+    onPressCallback = callback; // Store the callback
+    return true;  // Return true to indicate a callback was set
+  }
+
+  bool released(std::function<void()> callback) {
+    onPressCallback = callback; // Store the callback
+    return true;  // Return true to indicate a callback was set
+  }
+
+  void trigger() {
+    // Simulate a button press by calling the callback
+    if (onPressCallback) {
+      onPressCallback();
+    }
+  }
+};
+
+class axis {
+public:
+  int position() { return 0; }
+};
+// Class representing the controller with buttons
+class controller {
+public:
+  button ButtonRUp;
+  button ButtonRDown;
+  button ButtonLUp;
+  button ButtonLDown;
+  button ButtonEUp;
+  button ButtonEDown;
+  button ButtonFUp;
+  button ButtonFDown;
+  button ButtonL3;
+  button ButtonR3;
+
+  axis AxisA;
+  axis AxisB;
+  axis AxisC;
+  axis AxisD;
+
+  controller() = default;
+  
+  // You could add methods for each button to register callbacks
+  void registerButtonRUpPress(std::function<void()> callback) {
+    ButtonRUp.pressed(callback);
+  }
+
+  void registerButtonRUpRelease(std::function<void()> callback) {
+    ButtonRUp.released(callback);
+  }
+
+  // Simulate button press
+  void simulateButtonRUpPress() {
+    ButtonRUp.trigger();  // Trigger the callback when RUp button is pressed
+  }
+};
+
+// Class representing a motor
+class motor {
+private:
+  port portnumber;
+  bool reversed;
+  stopping stopp;
+
+public:
+  motor(port portnumber, bool reversed = false) {
+    this->portnumber = portnumber;
+    this->reversed = reversed;
+  }
+
+  motor(port portnumber, double gearRatio, bool reversed = false) {}
+
+  void spin(directionType direction) {}
+  
+  void spin(directionType direction, double speed, speedUnit unit) {}
+
+  void setMaxTorque(double max, speedUnit unit) {}
+
+  void setVelocity(double max, speedUnit unit) {}
+
+  void setStopping(stopping sto) { stopp = sto; }
+
+  void resetPosition() {}
+  double position(degreesUnits unit) { return 0.0; }
+  void stop() {}
+};
+
+
+class timer {
+public:
+  // no constuctor
+
+  void reset() {}
+
+  double value() { return 0.0; }
+};
+
+// Class representing the brain (main processing unit of the robot)
+class brain {
+public:
+  brain() = default;
+
+  timer Timer;
+  button buttonRight;
+  button buttonLeft;
+  void playSound(sound ooh) {}
+};
+
+// Class representing a pneumatic device
+class pneumatic {
+private:
+  port portnumber;
+
+public:
+  pneumatic(port portnumber) {
+    this->portnumber = portnumber;
+  }
+
+  void extend(cylinder cyl) {}
+  void retract(cylinder cyl) {}
+
+  void pumpOn() {}
+  void pumpOff() {}
+};
+
+// Class representing a touch LED
+class touchled {
+private:
+  port portnumber;
+
+public:
+  touchled(port portnumber) {
+    this->portnumber = portnumber;
+  }
+  bool pressing() { return 0; }
+};
+
+class motor_group {
+private:
+  motor motor1;
+  motor motor2;
+
+public:
+  // Constructor that initializes two motors with specified ports and options
+  motor_group(motor motor1, motor motor2) : motor1(motor1), motor2(motor2) { }
+
+  void spin(directionType direction) {}
+  
+  void spin(directionType direction, double speed, speedUnit unit) {}
+
+  void setMaxTorque(double max, speedUnit unit) {}
+
+  void setVelocity(double max, speedUnit unit) {}
+
+  void setStopping(stopping sto) { stopp = sto; }
+
+  void resetPosition() {}
+  double position(degreesUnits unit) { return 0.0; }
+  void stop() {}
+
+
+  double velocity(speedUnit unit) {return 0.0; }
+};
+
+class bumper {
+private:
+  port portnumber;
+
+public:
+  bumper(port portnumber) : portnumber(portnumber) { }
+
+  bool pressing() { return false; }
+};
+
+class distance {
+private:
+  port portnumber;
+
+public:
+  distance(port portnumber) : portnumber(portnumber) { }
+  double objectDistance(distanceUnits unit) { return 0.0; }
+};
+
+class gyro {
+private:
+  port portnumber;
+
+public:
+  gyro(port portnumber) : portnumber(portnumber) { }
+};
+
+class thread {
+public:
+  thread(std::function<void()> callback) {}
+};
+
+
+
+class sonar {
+private:
+  port portnumber;
+
+public:
+  sonar(port portnumber) : portnumber(portnumber) {}
+
+  double distance(distanceUnits unit) { return 0.0; }
+
+  u_int32_t timestamp() { return 0; }
+  
+ 
+};
+// Wait function that waits for a specified time
+void wait(double time, timeUnit unit) {}
+
+
+} // namespace vex
